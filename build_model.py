26f72cf23991d855d725ad9654d7fc98ad635737
gSchool Student <gSchool@Datas-Mac-mini-19.local>
2015-06-16 19:03:33 -0700

    Suggesting collaborators functionatlity added

--- build_model.py
+++ build_model.py
@@ -7,6 +7,7 @@ import numpy as np
 from string import punctuation, maketrans
 from gensim.models.word2vec import Word2Vec
 from gensim.models.doc2vec import Doc2Vec, LabeledSentence
+from get_collaborators import FindCollaborators
 
 
 class BuildReadmeModel(object):
@@ -26,7 +27,6 @@ class BuildReadmeModel(object):
         doc = doc.encode('utf-8')
         cleaned_doc = doc.translate(maketrans("", ""), punctuation)
         return cleaned_doc.lower()
-
 
     def get_readmes(self):
         repos = list(self.database.find({}, {"readme": 1}))
@@ -85,68 +85,70 @@ class BuildReadmeModel(object):
 
         return self.readmes[best_fit]
 
+    def suggest_collaborators(self):
+
 
 if __name__ == '__main__':
     readme_model = BuildReadmeModel()
     readme_model.get_readmes()
     readme_model.build_model()
 
-    query = """You didn't write that awful page. You're just trying to get some data out of it. Beautiful Soup is here to help. Since 2004, it's been saving programmers hours or days of work on quick-turnaround screen scraping projects.
+    query = """Note: We're using Kramdown Markdown extensions, such as definition lists.
 
-Beautiful Soup
+JSON Responses
 
-"A tremendous boon." -- Python411 Podcast
+We specify the JSON responses in Ruby so that we don't have to write them by hand all over the docs. You can render the JSON for a resource like this:
 
-[ Download | Documentation | Hall of Fame | Source | Discussion group ]
+<%= json :issue %>
+This looks up GitHub::Resources::ISSUE in lib/resources.rb.
 
-If Beautiful Soup has saved you a lot of time and money, the best way to pay me back is to check out Constellation Games, my sci-fi novel about alien video games.
-You can read the first two chapters for free, and the full novel starts at 5 USD. Thanks!
-If you have questions, send them to the discussion group. If you find a bug, file it.
+Some actions return arrays. You can modify the JSON by passing a block:
 
-Beautiful Soup is a Python library designed for quick turnaround projects like screen-scraping. Three features make it powerful:
+<%= json(:issue) { |hash| [hash] } %>
+Terminal blocks
 
-Beautiful Soup provides a few simple methods and Pythonic idioms for navigating, searching, and modifying a parse tree: a toolkit for dissecting a document and extracting what you need. It doesn't take much code to write an application
-Beautiful Soup automatically converts incoming documents to Unicode and outgoing documents to UTF-8. You don't have to think about encodings, unless the document doesn't specify an encoding and Beautiful Soup can't detect one. Then you just have to specify the original encoding.
-Beautiful Soup sits on top of popular Python parsers like lxml and html5lib, allowing you to try out different parsing strategies or trade speed for flexibility.
-Beautiful Soup parses anything you give it, and does the tree traversal stuff for you. You can tell it "Find all the links", or "Find all the links of class externalLink", or "Find all the links whose urls match "foo.com", or "Find the table heading that's got bold text, then give me that text."
+You can specify terminal blocks with pre.terminal elements. (It'd be nice if Markdown could do this more cleanly.)
 
-Valuable data that was once locked up in poorly-designed websites is now within your reach. Projects that would have taken hours take only minutes with Beautiful Soup.
-
-Interested? Read more.
-
-Download Beautiful Soup
-
-The current release is Beautiful Soup 4.3.2 (October 2, 2013). You can install it with pip install beautifulsoup4 or easy_install beautifulsoup4. It's also available as the python-beautifulsoup4 package in recent versions of Debian, Ubuntu, and Fedora .
-
-Beautiful Soup 4 works on both Python 2 (2.6+) and Python 3.
-
-Beautiful Soup is licensed under the MIT license, so you can also download the tarball, drop the bs4/ directory into almost any Python application (or into your library path) and start using it immediately. (If you want to do this under Python 3, you will need to manually convert the code using 2to3.)
-
-Beautiful Soup 3
-
-Beautiful Soup 3 was the official release line of Beautiful Soup from May 2006 to March 2012. It is considered stable, and only critical bugs will be fixed. Here's the Beautiful Soup 3 documentation.
-
-Beautiful Soup 3 works only under Python 2.x. It is licensed under the same license as Python itself.
-
-The current release of Beautiful Soup 3 is 3.2.1 (February 16, 2012). You can install Beautiful Soup 3 with pip install BeautifulSoup or easy_install BeautifulSoup. It's also available as python-beautifulsoup in Debian and Ubuntu, and as python-BeautifulSoup in Fedora.
-
-You can also download the tarball and use BeautifulSoup.py in your project directly.
-
-Hall of Fame
-
-Over the years, Beautiful Soup has been used in hundreds of different projects. There's no way I can list them all, but I do want to highlight a few high-profile projects. Beautiful Soup isn't what makes these projects interesting, but it did make their completion easier:
-
-"Movable Type", a work of digital art on display in the lobby of the New York Times building, uses Beautiful Soup to scrape news feeds.
-Reddit uses Beautiful Soup to parse a page that's been linked to and find a representative image.
-Alexander Harrowell uses Beautiful Soup to track the business activities of an arms merchant.
-The developers of Python itself used Beautiful Soup to migrate the Python bug tracker from Sourceforge to Roundup.
-The Lawrence Journal-World uses Beautiful Soup to gather statewide election results.
-The NOAA's Forecast Applications Branch uses Beautiful Soup in TopoGrabber, a script for downloading "high resolution USGS datasets."
-If you've used Beautiful Soup in a project you'd like me to know about, please do send email to me or the discussion group.
+<pre class="terminal">
+$ curl foobar
+....
+</pre>
+This is not a curl tutorial though. Not every API call needs to show how to access it with curl.
 
 Development
 
-Development happens at Launchpad. You can get the source code or file bugs.
+Nanoc compiles the site into static files living in ./output. It's smart enough not to try to compile unchanged files:
+
+$ bundle exec nanoc compile
+Loading site data...
+Compiling site...
+   identical  [0.00s]  output/css/960.css
+   identical  [0.00s]  output/css/pygments.css
+   identical  [0.00s]  output/css/reset.css
+   identical  [0.00s]  output/css/styles.css
+   identical  [0.00s]  output/css/uv_active4d.css
+      update  [0.28s]  output/index.html
+      update  [1.31s]  output/v3/gists/comments/index.html
+      update  [1.92s]  output/v3/gists/index.html
+      update  [0.25s]  output/v3/issues/comments/index.html
+      update  [0.99s]  output/v3/issues/labels/index.html
+      update  [0.49s]  output/v3/issues/milestones/index.html
+      update  [0.50s]  output/v3/issues/index.html
+      update  [0.05s]  output/v3/index.html
+
+Site compiled in 5.81s.
+You can setup whatever you want to view the files. If using the adsf gem (as listed in the Gemfile), you can start Webrick:
+
+$ bundle exec nanoc view
+$ open http://localhost:3000
+Compilation times got you down? Use autocompile!
+
+$ bundle exec nanoc autocompile
+This starts a web server too, so there's no need to run nanoc view. One thing: remember to add trailing slashes to all nanoc links!
+
+Deploy
+
+$ bundle exec rake publish
 
 """
     print readme_model.make_recommendation(query)
---
+++ get_collaborators.py
@@ -0,0 +1,24 @@
+import requests
+import os
+from collections import Counter
+
+class FindCollaborators(object):
+
+    def __init__(self, repo_url, n_results=3):
+        self.repo_url = repo_url
+        self.auth = ('viknat', os.environ['GITHUB_ACCESS_TOKEN'])
+
+    def get_collaborators(self):
+        pull_url = self.repo_url + "/pulls" + "?state=\"closed\""
+        r = requests.get(pull_url, auth=self.auth)
+
+        self.pullees = r.json()
+        best_collabs = Counter([pullee['user']['html_url'] for pullee in self.pullees])
+        return best_collabs.most_common(3)
+
+if __name__ == '__main__':
+    collab_finder = FindCollaborators(
+        repo_url="https://api.github.com/repos/kennethreitz/requests")
+
+    collab_finder.get_collaborators()
+
